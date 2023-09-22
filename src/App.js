a1197e953eadd2fe558ef3b8b6ddb20c97173830
arjybltrn <arjaybeltran16@gmail.com>
2023-09-22 16:31:44 -0400

    exp: added professional experience

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { Link, Outlet } from "react-router-dom"
 
 import React from 'react';
 
-const about = ""
+const about = "Skilled full-stack developer with expertise in HTML, CSS, JavaScript, React, Python, Django, Express, PostgreSQL, MongoDB, Node.js, and Git. Holds Graduate Certificates in Software Engineering and Home Construction Renovation, and an Advanced Diploma in Computer Science. "
+
+
 const projects = [
     {
         projectTitle: "//HLLO_WRLD",
@@ -37,6 +39,68 @@ const projects = [
     },
 ]
 
+
+const exp = [ 
+    {
+        companyName: "General Assembly",
+        jobTitle: "Software Engineer Fellow",
+        location: "Remote",
+        jobDesc: [ 
+                    "Created full-stack applications with comprehensive functionality, including Create, Read, Update, and Delete (CRUD) operations and RESTful API capabilities.",
+                    "Utilized JavaScript and React for front-end web development, incorporating third-party APIs seamlessly.", 
+                    "Employed Python and Django for back-end development, utilizing PostgreSQL, MongoDB, and Express for efficient data management.",
+                    "Successfully completed over 450 hours in the intensive Software Engineering Immersive program."
+    ],
+        date: "March 2023 - May 2023"
+
+    },
+    {
+        companyName: "Madison Group",
+        jobTitle: "Customer Care Technician",
+        location: "Toronto, ON",
+        jobDesc: [ 
+                    "Consistently upheld a professional demeanor while successfully fulfilling work orders and service calls as the company's representative.",
+                    "Excelled in inspections, diagnoses, repairs, and documentation, emphasizing strong problem-solving and attention to detail.", 
+    ],
+        date: "March 2022 - March 2023"
+
+    },
+    {
+        companyName: "R-MAC Solutions, Inc.",
+        jobTitle: "Junior Carpenter",
+        location: "Toronto, ON",
+        jobDesc: [ 
+                    "Applied a wide range of effective problem-solving approaches and tools to consistently achieve project deadlines and deliver high-quality results.",
+                    "Exhibited strong organization in project planning, adhering to codes and safety, enhancing client satisfaction.", 
+    ],
+        date: "October 2021 - February 2022"
+
+    },
+    {
+        companyName: "G Hirsch Renovations",
+        jobTitle: "Apprentice Carpenter",
+        location: "Toronto",
+        jobDesc: [ 
+                    "Proficiently organized job sites, efficiently gathering tools and materials, while upholding high standards of cleanliness and safety.",
+                    "Aided carpenters in structure installation, displayed problem-solving skills, and maintained workspace.", 
+    ],
+        date: "June 2021 - November 2021"
+
+    },
+    {
+        companyName: "UNIQLO Canada",
+        jobTitle: "Assistant Store Manager",
+        location: "Toronto",
+        jobDesc: [ 
+                    "Successfully achieved daily store sales target of 100% through strategic planning and effective sales management.",
+                    "Oversaw store operations, prioritizing safety and excellent customer service.", 
+    ],
+        date: "August 2017 - July 2020"
+
+    },
+
+]
+
 function Home(props) {
     return (
         <div>
@@ -65,8 +129,8 @@ export function About(props) {
     );
 }
 
-export function Projects(props) {
-    return (
+export function Projects({ data, renderItem, renderEmpty}) {
+    return !data.length ? renderEmpty : 
         <div>
             <nav>
                 <Link to="/">Home</Link>
@@ -76,8 +140,9 @@ export function Projects(props) {
             </nav>
             <h1>Projects</h1>
         </div>
-    );
 }
+
+
 
 export function Experiences(props) {
     return (
--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App, Experiences, About, Projects } from './App';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+// import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+// const client = new ApolloClient({
+//   uri: 'https://flyby-router-demo.herokuapp.com/',
+//   cache: new InMemoryCache(),
+// });
+
+// // const client = ...
+
+// client
+//   .query({
+//     query: gql`
+//       query GetLocations {
+//         locations {
+//           id
+//           name
+//           description
+//           photo
+//         }
+//       }
+//     `,
+//   })
+//   .then((result) => console.log(result));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
