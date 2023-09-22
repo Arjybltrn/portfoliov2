9a1193c56c775559a94e5edeb900fa1d6f14c3dd
arjybltrn <arjaybeltran16@gmail.com>
2023-09-22 15:59:28 -0400

    hardcode project list

--- src/App.js
+++ src/App.js
@@ -3,6 +3,40 @@ import { Link, Outlet } from "react-router-dom"
 
 import React from 'react';
 
+const about = ""
+const projects = [
+    {
+        projectTitle: "//HLLO_WRLD",
+        desc: "Discover basic information about any country by searching for it. Explore details such as population, languages spoken, currency, and more.",
+        tech_stack: [ "JavaScript", "HTML", "CSS", "AJAX" ],
+        ss_img: ""
+    },
+    {
+        projectTitle: "docket.",
+        desc: "Stay organized with a simple and intuitive to-do list application. Keep track of your tasks, set reminders, and easily manage your daily agenda.",
+        tech_stack: [ "JavaScript", "HTML", "CSS", "Express.js" ],
+        ss_img: ""
+    },
+    {
+        projectTitle: "Choruso",
+        desc: "Immense yourself in the world of musiic with Choruso, a collaborative music application. Discover, listen to, and share your favourite songs and playlists with friends.",
+        tech_stack: [ "Python", "Django", "PostgreSQL" ],
+        ss_img: ""
+    },
+    {
+        projectTitle: "Trackly",
+        desc: "Simplify your job application process with Trackly, a comprehensive job application tracker. Keep tabs on your applications, track progress, and stay organized in your job search",
+        tech_stack: [ "MongoDB", "Express.js", "React", "Node.js" ],
+        ss_img: ""
+    },
+    {
+        projectTitle: "flowerbox",
+        desc: "Your online destinatiiioin for exquisite floral arrangements and fresh blooms. Discover beauty, elegance, and joy in every petal.",
+        tech_stack: [ "React", "Node.js", "CSS" ],
+        ss_img: ""
+    },
+]
+
 function Home(props) {
     return (
         <div>
@@ -12,7 +46,7 @@ function Home(props) {
                 <Link to="/projects">Projects</Link>
                 <Link to="/experiences">Professional Experiences</Link>
             </nav>
-            <h1>My Website</h1>
+            <h1>Arjay Beltran</h1>
         </div>
     );
 }
